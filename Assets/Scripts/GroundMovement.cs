d06be735428e699aed318b3621f661b3c57f8f77
Jeremiah <jermcdon18@gmail.com>
2025-03-25 11:32:57 -0500

    Tilemap
    Created two Tilemaps for the ground and have it loop endlessly to model running.

--- Assets/Scripts/GroundMovement.cs
+++ Assets/Scripts/GroundMovement.cs
@@ -3,16 +3,17 @@ using UnityEngine;
 public class GroundMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-    [SerializeField] private float resetPositionY = -25f;
-    [SerializeField] private float startPositionY = 25f;
+    [SerializeField] private float tileHeight = 72f;
+    //[SerializeField] private float resetPositionY = -25f;
+    //[SerializeField] private float startPositionY = 25f;
 
     private void Update()
     {
         transform.Translate(Vector3.down * speed * Time.deltaTime);
 
-        if(transform.position.y <= resetPositionY)
+        if(transform.position.y <= -tileHeight)
         {
-            transform.position = new Vector3(transform.position.x, startPositionY, transform.position.z);
+            transform.position = new Vector3(transform.position.x, tileHeight, 0);
         }
     }
 }
